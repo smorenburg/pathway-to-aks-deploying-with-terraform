977983b3829cd5d962ecc6a9ab422ad270736c29
Robin Smorenburg <robin.smorenburg@ericsson.com>
2023-02-10 17:57:29 +0100

    Define node pool configuration

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,14 +1,20 @@
-resource "azurerm_kubernetes_cluster" "example" {
-  name                 = "cluster-${var.environment}-aks-${local.location_name}"
+# Create the Kubernetes cluster, including the default node pool.
+resource "azurerm_kubernetes_cluster" "default" {
+  name                 = "aks-${var.environment}-${local.app}-${local.location_lower}"
   location             = local.location
-  resource_group_name  = azurerm_resource_group.aks.name
-  dns_prefix           = "cluster-${var.environment}-aks-${local.location_name}"
+  resource_group_name  = azurerm_resource_group.default.name
+  node_resource_group  = "rg-${var.environment}-app-${local.location_lower}-aks-nodes"
+  dns_prefix           = "aks-${var.environment}-${local.app}-${local.location_lower}"
   azure_policy_enabled = true
 
   default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_D2_v5"
+    name                = "default"
+    node_count          = 3
+    vm_size             = "Standard_D2_v5"
+    zones               = ["1", "2", "3"]
+    enable_auto_scaling = true
+    min_count           = 3
+    max_count           = 6
   }
 
   identity {
