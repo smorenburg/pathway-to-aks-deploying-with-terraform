1e24eebfa158880fa950efd460ae47b2d8b01432
Robin Smorenburg <robin.smorenburg@ericsson.com>
2023-02-11 11:56:01 +0100

    Modify cluster name

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,10 +1,10 @@
 # Create the Kubernetes cluster, including the default node pool.
 resource "azurerm_kubernetes_cluster" "default" {
-  name                 = "aks-${local.name_suffix}"
+  name                 = "aks-cluster-${local.name_suffix}"
   location             = var.location
   resource_group_name  = azurerm_resource_group.default.name
   node_resource_group  = "${azurerm_resource_group.default.name}-aks-nodes"
-  dns_prefix           = "aks-${local.name_suffix}"
+  dns_prefix           = "aks-cluster-${local.name_suffix}"
   azure_policy_enabled = true
 
   default_node_pool {
