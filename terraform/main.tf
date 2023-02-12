3a440b4018b1910af9026e253d9247fee03b14f0
Robin Smorenburg <robin.smorenburg@ericsson.com>
2023-02-12 15:29:54 +0100

    Configure provider features

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,13 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
+
 
 locals {
   app                  = "app"
