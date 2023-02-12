4c4d64d54f560351f1412e3cd2fa3766c1f37070
Robin Smorenburg <robin.smorenburg@ericsson.com>
2023-02-12 15:05:51 +0100

    Add Log Analytics workspace

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,21 @@ provider "azurerm" {
 }
 
 locals {
-  app            = "app"
-  name_suffix    = "${local.app}-${var.environment}-${var.location}"
+  app                  = "app"
+  name_suffix          = "${local.app}-${var.environment}-${var.location}"
+  authorized_ip_ranges = ["77.169.37.43/32"]
 }
 
-# Create the resource group..
+# Create the resource group.
 resource "azurerm_resource_group" "default" {
   name     = "rg-${local.name_suffix}"
   location = var.location
+}
+
+# Create the Log Analytics workspace.
+resource "azurerm_log_analytics_workspace" "default" {
+  name                = "log-${local.name_suffix}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.default.name
+  retention_in_days   = 30
 }
